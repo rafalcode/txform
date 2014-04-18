4767c5686ba8447b7f34b1ccae9e33eca64dd789
Ramon F <ramonfallon@gmail.com>
2014-04-18 23:04:07 +0200

    introduce numchar per line array

--- txform.c
+++ txform.c
@@ -3,6 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CABUF 2
+/* Reminder: a is the array whose size is being incremented, and t is the type, a string  */
+#define CONDREALLOC(x, b, c, a, t); \
+        if((x)==((b)-1)) { \
+                    (b) += (c); \
+                    (a)=realloc((a), (b)*sizeof(t)); \
+                }
+
 int main(int argc, char *argv[])
 {
    /* argument accounting: remember argc, the number of arguments, _includes_ the executable */
@@ -12,19 +20,31 @@ int main(int argc, char *argv[])
 	}
 
    FILE *fin=fopen(argv[1], "r");
-   int c;
-   unsigned nc /* numchars */=0, nl /*num lines */=0;
+   int i, c;
+   unsigned nc /* numchars */=0, oldnc=0, nl /*num lines */=0;
+   unsigned *ncpla /* numchar per line array */=NULL, ncpla_buf=CABUF;
+   ncpla=malloc(ncpla_buf*sizeof(unsigned));
 
    for(;;) {
        c=fgetc(fin);
        nc++;
        if(c==EOF) break;
-       if(c=='\n') nl++;
+       if(c=='\n') {
+           CONDREALLOC(nl, ncpla_buf, CABUF, ncpla, unsigned);
+           ncpla[nl++]=nc-oldnc-1; /* minus one so not to include newlines */
+           oldnc=nc;
+       }
    }
+   ncpla=realloc(ncpla, nl*sizeof(unsigned)); /*normalize*/
 
    fclose(fin);
-
    printf("Report for file \"%s\" - num chars: %u, numlines: %u\n", argv[1], nc, nl);
+   printf("numchar per line array is: ");
+   for(i=0;i<nl;++i) 
+       printf("%u ", ncpla[i]);
+   printf("\n"); 
+
+   free(ncpla);
 
    return 0;
 }
