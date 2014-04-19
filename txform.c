5f5fcd6499ccaff7a15b6fccc4d242fa6471ca3b
Ramonf <ramonfallon@gmail.com>
2014-04-19 16:38:19 +0200

    finished adding word count and last word holder

--- txform.c
+++ txform.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define CABUF 2
+#define WBUF 8 /* word buffer */
 /* Reminder: a is the array whose size is being incremented, and t is the type, a string  */
 #define CONDREALLOC(x, b, c, a, t); \
         if((x)==((b)-1)) { \
@@ -21,29 +22,45 @@ int main(int argc, char *argv[])
 
    FILE *fin=fopen(argv[1], "r");
    int i, c;
+   char oldc=' ';
    unsigned nc /* numchars */=0, oldnc=0, nl /*num lines */=0;
    unsigned *ncpla /* numchar per line array */=NULL, ncpla_buf=CABUF;
    ncpla=malloc(ncpla_buf*sizeof(unsigned));
+   char *lsw /* last seen word */, lwbuf=WBUF;
+   lsw=calloc(lwbuf, sizeof(char));
+   unsigned wc=0 /* word-character count */, gwc=0 /* general word count */;
 
    for(;;) {
        c=fgetc(fin);
-       nc++;
        if(c==EOF) break;
+       nc++; /* having this just after EOF detector means EOF is not coutned as a character, which is the convention */
+       if( ((c == ' ') || (c == '\n') || (c == '\t')) && ((oldc != ' ') & (oldc != '\n') & (oldc != '\t'))) {
+           lsw[wc]='\0';
+           gwc++;
+           printf("%u) lw=\"%s\";sz=%u\n", gwc, lsw, wc);
+           wc=0;
+       } else if( (c != ' ') & (c != '\n') & (c != '\t')) {
+           CONDREALLOC(wc, lwbuf, WBUF, lsw, char);
+           lsw[wc++]=c;
+       }
+
        if(c=='\n') {
            CONDREALLOC(nl, ncpla_buf, CABUF, ncpla, unsigned);
            ncpla[nl++]=nc-oldnc-1; /* minus one so not to include newlines */
            oldnc=nc;
        }
+       oldc=c;
    }
    ncpla=realloc(ncpla, nl*sizeof(unsigned)); /*normalize*/
 
    fclose(fin);
-   printf("Report for file \"%s\" - num chars: %u, numlines: %u\n", argv[1], nc, nl);
+   printf("Report for file \"%s\" - numchars: %u, nwords= %u, numlines: %u\n", argv[1], nc, gwc, nl);
    printf("numchar per line array is: ");
    for(i=0;i<nl;++i) 
        printf("%u ", ncpla[i]);
    printf("\n"); 
 
+   free(lsw);
    free(ncpla);
 
    return 0;
