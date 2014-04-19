f3006c0c1962da0c0e0ad4c7aa141b77796b4afb
Ramonf <ramonfallon@gmail.com>
2014-04-19 21:20:16 +0200

    line array has turned into a struct ... recording gboth numchars and numwords per line correctly

--- txform.c
+++ txform.c
@@ -7,10 +7,16 @@
 #define WBUF 8 /* word buffer */
 /* Reminder: a is the array whose size is being incremented, and t is the type, a string  */
 #define CONDREALLOC(x, b, c, a, t); \
-        if((x)==((b)-1)) { \
+        if((x)==(b)) { \
                     (b) += (c); \
                     (a)=realloc((a), (b)*sizeof(t)); \
                 }
+
+typedef struct
+{
+    unsigned ccou;
+    unsigned wcou;
+} lndats; /* line data */
 
 int main(int argc, char *argv[])
 {
@@ -24,11 +30,14 @@ int main(int argc, char *argv[])
    int i, c;
    char oldc=' ';
    unsigned nc /* numchars */=0, oldnc=0, nl /*num lines */=0;
-   unsigned *ncpla /* numchar per line array */=NULL, ncpla_buf=CABUF;
-   ncpla=malloc(ncpla_buf*sizeof(unsigned));
+   lndats **ncpla /* numchar per line array */;
+   unsigned ncpla_buf=CABUF;
+   ncpla=malloc(ncpla_buf*sizeof(lndats*));
+   for(i=0;i<ncpla_buf;++i) 
+        ncpla[i]=calloc(1, sizeof(lndats));
    char *lsw /* last seen word */, lwbuf=WBUF;
    lsw=calloc(lwbuf, sizeof(char));
-   unsigned wc=0 /* word-character count */, gwc=0 /* general word count */;
+   unsigned wc=0 /* word-character count */, gwc=0 /* general word count */, oldgwc=0;
 
    for(;;) {
        c=fgetc(fin);
@@ -45,22 +54,34 @@ int main(int argc, char *argv[])
        }
 
        if(c=='\n') {
-           CONDREALLOC(nl, ncpla_buf, CABUF, ncpla, unsigned);
-           ncpla[nl++]=nc-oldnc-1; /* minus one so not to include newlines */
+           if(nl==ncpla_buf) {
+               ncpla_buf += CABUF;
+               ncpla=realloc(ncpla, ncpla_buf*sizeof(lndats*));
+               for(i=ncpla_buf-CABUF;i<ncpla_buf; ++i) 
+                   ncpla[i]=calloc(1, sizeof(lndats));
+           }
+           ncpla[nl]->ccou=nc-oldnc-1; /* minus one so not to include newlines */
+           ncpla[nl]->wcou=gwc-oldgwc; /* minus one so not to include newlines */
+           nl++;
            oldnc=nc;
+           oldgwc=gwc;
        }
        oldc=c;
    }
-   ncpla=realloc(ncpla, nl*sizeof(unsigned)); /*normalize*/
+   for(i=nl;i<ncpla_buf;++i) 
+       free(ncpla[i]);
+   ncpla=realloc(ncpla, nl*sizeof(lndats*)); /*normalize*/
 
    fclose(fin);
    printf("Report for file \"%s\" - numchars: %u, nwords= %u, numlines: %u\n", argv[1], nc, gwc, nl);
    printf("numchar per line array is: ");
    for(i=0;i<nl;++i) 
-       printf("%u ", ncpla[i]);
+       printf("%u,%u|", ncpla[i]->ccou, ncpla[i]->wcou);
    printf("\n"); 
 
    free(lsw);
+   for(i=0;i<nl;++i) 
+       free(ncpla[i]);
    free(ncpla);
 
    return 0;
